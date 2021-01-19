96016082282863baf0be6712b7a670cb2de74a40
Kesavan Yogeswaran <kesyog@gmail.com>
2021-01-19 00:47:58 -0500

    Reduce unsafe usage to the bare minimum
    * Removed some unecessary unsafe usage
    * Use unsafe as early as possible when access unions to avoid having to
    litter the code
    
    Unsafe is only used for three types of operations:
    * Calling into the C SDK
    * Accessing unions returned by the SDK
    * Logging via ITM from multiple places without needing to pass the
    peripheral around
    
    The first two are impossible to remove without rewriting the SDK and the
    third is used as a low-consequence convenience for logging.

--- src/nrf_verify_transport.rs
+++ src/nrf_verify_transport.rs
@@ -1,6 +1,5 @@
 /// Test project to verify SPI/ACI by sending echo commands to the nRF8001
 /// This is a port of the ble_aci_transport_layer_verification project from the Nordic SDK
-
 use crate::delay;
 use crate::io_interface::IOPin;
 use crate::ITM_HANDLE;
@@ -8,7 +7,8 @@ use cortex_m::iprintln;
 use nrf8001_sys::{
     aci_device_operation_mode_t_ACI_DEVICE_SETUP, aci_device_operation_mode_t_ACI_DEVICE_STANDBY,
     aci_device_operation_mode_t_ACI_DEVICE_TEST, aci_evt_opcode_t_ACI_EVT_CMD_RSP,
-    aci_evt_opcode_t_ACI_EVT_DEVICE_STARTED, aci_evt_opcode_t_ACI_EVT_ECHO, aci_evt_t, aci_pins_t,
+    aci_evt_opcode_t_ACI_EVT_DEVICE_STARTED, aci_evt_opcode_t_ACI_EVT_ECHO,
+    aci_evt_params_cmd_rsp_t, aci_evt_params_device_started_t, aci_evt_params_echo_t, aci_pins_t,
     aci_state_t, aci_status_code_t_ACI_STATUS_SUCCESS,
     aci_test_mode_change_t_ACI_TEST_MODE_DTM_UART, hal_aci_evt_t,
 };
@@ -55,19 +55,20 @@ impl Context {
 }
 
 pub fn poll(ctx: &mut Context) {
-    let has_message =
-        unsafe { nrf8001_sys::lib_aci_event_get(&mut ctx.aci_state, &mut ctx.aci_data) };
-    if has_message {
+    if unsafe { nrf8001_sys::lib_aci_event_get(&mut ctx.aci_state, &mut ctx.aci_data) } {
         match ctx.aci_data.evt.evt_opcode {
             aci_evt_opcode_t_ACI_EVT_DEVICE_STARTED => aci_device_started(
                 &mut ctx.aci_state,
-                &mut ctx.aci_data.evt,
+                unsafe { &ctx.aci_data.evt.params.device_started },
                 &mut ctx.n_aci_echo_cmds,
             ),
-            aci_evt_opcode_t_ACI_EVT_CMD_RSP => aci_cmd_rsp(&mut ctx.aci_data.evt),
-            aci_evt_opcode_t_ACI_EVT_ECHO => {
-                aci_echo(&mut ctx.aci_data.evt, &mut ctx.n_aci_echo_cmds)
+            aci_evt_opcode_t_ACI_EVT_CMD_RSP => {
+                aci_cmd_rsp(unsafe { &ctx.aci_data.evt.params.cmd_rsp })
             }
+            aci_evt_opcode_t_ACI_EVT_ECHO => aci_echo(
+                unsafe { &ctx.aci_data.evt.params.echo },
+                &mut ctx.n_aci_echo_cmds,
+            ),
             _ => {
                 let itm = unsafe { ITM_HANDLE.as_mut().unwrap() };
                 iprintln!(
@@ -91,13 +92,13 @@ fn send_echo_cmd(n_aci_echo_cmds: &mut u8) {
 
 fn aci_device_started(
     aci_state: &mut aci_state_t,
-    aci_evt: &mut aci_evt_t,
+    payload: &aci_evt_params_device_started_t,
     n_aci_echo_cmds: &mut u8,
 ) {
     let itm = unsafe { ITM_HANDLE.as_mut().unwrap() };
-    aci_state.data_credit_available = unsafe { aci_evt.params.device_started.credit_available };
+    aci_state.data_credit_available = payload.credit_available;
     iprintln!(&mut itm.stim[0], "Device started opcode");
-    match unsafe { aci_evt.params.device_started.device_mode } {
+    match payload.device_mode {
         aci_device_operation_mode_t_ACI_DEVICE_SETUP => {
             iprintln!(&mut itm.stim[0], "Evt Device Started: Setup");
             unsafe { nrf8001_sys::lib_aci_test(aci_test_mode_change_t_ACI_TEST_MODE_DTM_UART) };
@@ -119,28 +120,27 @@ fn aci_device_started(
             delay::delay(4000);
             send_echo_cmd(n_aci_echo_cmds);
         }
-        _ => iprintln!(&mut itm.stim[0], "Unexpected device_mode: {}", unsafe {
-            aci_evt.params.device_started.device_mode
-        }),
+        _ => iprintln!(
+            &mut itm.stim[0],
+            "Unexpected device_mode: {}",
+            payload.device_mode
+        ),
     }
 }
 
-fn aci_cmd_rsp(aci_evt: &mut aci_evt_t) {
+fn aci_cmd_rsp(payload: &aci_evt_params_cmd_rsp_t) {
     let itm = unsafe { ITM_HANDLE.as_mut().unwrap() };
     iprintln!(&mut itm.stim[0], "Response opcode");
-    if unsafe { aci_evt.params.cmd_rsp.cmd_status != aci_status_code_t_ACI_STATUS_SUCCESS } {
-        iprintln!(&mut itm.stim[0], "ACI Command 0x{:X}", unsafe {
-            aci_evt.params.cmd_rsp.cmd_opcode
-        });
+    if payload.cmd_status != aci_status_code_t_ACI_STATUS_SUCCESS {
+        iprintln!(&mut itm.stim[0], "ACI Command 0x{:X}", payload.cmd_opcode);
         panic!("Evt cmd response: error");
     }
 }
 
-fn aci_echo(aci_evt: &mut aci_evt_t, n_aci_echo_cmds: &mut u8) {
+fn aci_echo(payload: &aci_evt_params_echo_t, n_aci_echo_cmds: &mut u8) {
     let itm = unsafe { ITM_HANDLE.as_mut().unwrap() };
     iprintln!(&mut itm.stim[0], "Echo opcode");
-    let returned =
-        unsafe { core::slice::from_raw_parts(&aci_evt.params.echo.echo_data[0], ECHO_DATA.len()) };
+    let returned = &payload.echo_data[..ECHO_DATA.len()];
     if returned != ECHO_DATA {
         iprintln!(&mut itm.stim[0], "Echo mismatch: {:?}", returned);
     } else {
